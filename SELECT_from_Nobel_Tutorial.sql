7964d00ed1ec6a59fcf27988e87835deb2dcac51
othmane <othman-19@hotmail.fr>
2019-06-19 21:25:54 +0100

    FINSH WITH SELECT IN SELECT SQL TUTORIAL

--- SELECT_from_Nobel_Tutorial.sql
+++ SELECT_from_Nobel_Tutorial.sql
@@ -76,9 +76,9 @@ ORDER BY yr DESC, winner
 --Show the 1984 winners and subject ordered by subject and winner name;
 --but list Chemistry and Physics last.
 SELECT winner, subject
-  FROM nobel
- WHERE yr=1984
- ORDER BY subject IN ('Physics','Chemistry'),subject,winner
+FROM nobel
+WHERE yr=1984
+ORDER BY subject IN ('Physics', 'Chemistry'),subject,winner;
 
 
 
